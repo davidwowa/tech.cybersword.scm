e87cc940ecfc2f0d0ceeb75ce2d801fd10a8bfba
David Zakrevskyy <dev@cybersword.tech>
2024-11-06 00:09:31 +0100

    -nc

--- tex/create_pdf.sh
+++ tex/create_pdf.sh
@@ -15,7 +15,7 @@ pdflatex -jobname=$output_file $input_file
 if [ "$copy" == "true" ]; then
     mkdir -p backup
     cp "${output_file}.pdf" backup/
-    cp "${output_file}.tex" ~/Library/Mobile\ Documents/com~apple~CloudDocs/scm/
+    cp "${output_file}.pdf" ~/Library/Mobile\ Documents/com~apple~CloudDocs/scm/
 fi
 
 rm *.aux *.log
